949a2d43439f5b657338b24427617480de0f2b8d
jonatan de oliveira <j.jhonis95@gmail.com>
2023-08-29 10:43:11 -0300

    resolve crash

--- src/templates/cardContainer.js
+++ src/templates/cardContainer.js
@@ -3,55 +3,65 @@ import * as styles from "../components/index.module.css"
 import { v4 as uuidv4 } from 'uuid';
 import { getImage, GatsbyImage } from "gatsby-plugin-image"
 import { graphql,useStaticQuery } from "gatsby"
-function Card({name,description,price,image}){
-    const img=getImage(image)
-    return(
-        <section className={styles.card__Container}>
-            <GatsbyImage image={img} alt={name+' image'} />
-            <div className={styles.card__text__container}>
-                <h3 className={styles.card__dish__name}>{name}</h3>
-                <p className={styles.card__dish__description}>{description}</p>
-                <p className={styles.card__dish__price}>{price}</p>
-            </div>
-        </section>
-    )
-}
+// function Card({name,description,price,image}){
+//     console.log(image.childrenImageSharp)
+//     // console.log(getImage(image.childrenImageSharp.gatsbyImageData.image))
+//     const img=image.childrenImageSharp
+//     return(
+//         <section className={styles.card__Container}>
+//             <GatsbyImage image={img} alt={name+' image'} />
+//             <div className={styles.card__text__container}>
+//                 <h3 className={styles.card__dish__name}>{name}</h3>
+//                 <p className={styles.card__dish__description}>{description}</p>
+//                 <p className={styles.card__dish__price}>{price}</p>
+//             </div>
+//         </section>
+//     )
+// }
 
 function CardContainer({activeMenu}){
   const query = useStaticQuery(graphql`
-  query MyQuery {
+  query MyQuery2 {
     data {
       restaurant {
         menu {
           appetizer {
-            price
             name
-            image
+            image {
+              childrenImageSharp {
+                gatsbyImageData
+              }
+            }
+            price
             descriprion
           }
           bowls {
-            price
-            name
-            image
             descriprion
+            name
+            price
+            image {
+              childrenImageSharp {
+                gatsbyImageData
+              }
+            }
           }
           curry {
-            price
-            name
-            image
             descriprion
+            name
+            price
+            image
           }
           dessert {
+            descriprion
             name
             price
             image
-            descriprion
           }
           drinks {
-            price
-            name
-            image
             descriprion
+            name
+            price
+            image
           }
         }
       }
@@ -84,14 +94,16 @@ function CardContainer({activeMenu}){
         <section className={styles.menu__appatizer__container}>
             {
                 dishes.map((appetizer)=>{
+                    console.log(getImage(query.data.restaurant.menu[0].appetizer.image))
                     return(
-                        <Card 
-                            image={appetizer.image}
-                            name={appetizer.name}
-                            description={appetizer.descriprion}
-                            price={appetizer.price}
-                            key={uuidv4()}
-                        />
+                        <section className={styles.card__Container} key={uuidv4}>
+                            <GatsbyImage image={getImage(appetizer.image)} alt={appetizer+' image'} />
+                            <div className={styles.card__text__container}>
+                                <h3 className={styles.card__dish__name}>{appetizer.name}</h3>
+                                <p className={styles.card__dish__description}>{appetizer.descriprion}</p>
+                                <p className={styles.card__dish__price}>{appetizer.price}</p>
+                            </div>
+                        </section>
                     )
                 })
             }
@@ -99,64 +111,3 @@ function CardContainer({activeMenu}){
     )
 }
 export default CardContainer;
-// function Card(){
-//     const{allFile} =useStaticQuery(graphql`
-//     query {
-//       allFile(filter: {id: {eq: "5e61f6a4-c59f-57f5-975f-b1fc97c312b6"}}) {
-//         nodes {
-//           name
-//         }
-//       }
-//     }
-//   `)
-//     console.log(allFile.nodes.name)
-//     return(
-//         <p>test</p>
-//     )
-// }
-// function CardContainer(){
-//     return(
-//         <section className={styles.menu__appatizer__container}>
-//             {/* {
-//                 appetizers.map((appetizer)=>{
-//                     return(
-//                         <Card 
-//                             image={appetizer.image}
-//                             name={appetizer.name}
-//                             description={appetizer.descriprion}
-//                             price={appetizer.price}
-//                             key={uuidv4()}
-//                         />
-//                     )
-//                 })
-//             } */}
-//             <Card/>
-//         </section>
-//     )
-// }
-// export default CardContainer
-// import { graphql,useStaticQuery } from "gatsby"
-// import React from "react"
-// // import { GatsbyImage, getImage } from "gatsby-plugin-image"
-
-// function CardContainer() {
-//     const data = useStaticQuery(graphql`
-//     query MyQuery {
-//       data {
-//         restaurant {
-//           about
-//         }
-//       }
-//     }
-// `)
-// //   console.log(typeof(data.restaurant.about))
-//   console.log(data)
-//   return (
-//     <section>
-//         <p>test</p>
-//       {/* <GatsbyImage image={getImage(data)} alt={data.allFiles.nodes[0].name} /> */}
-//       {/* <p>{data.restaurant.about}</p> */}
-//     </section>
-//   )
-// }
-// export default CardContainer
