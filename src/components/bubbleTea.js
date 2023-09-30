779d2c7495ecfb6686b7d91581ec488085c5771b
jonatan de oliveira <j.jhonis95@gmail.com>
2023-09-30 20:31:56 -0300

    finish the desktop version

--- src/components/bubbleTea.js
+++ src/components/bubbleTea.js
@@ -46,71 +46,90 @@ function BubbleTea(){
     const background=getImage(data.data.restaurant.bubbleTea.background.childrenImageSharp[0].gatsbyImageData);
     return(
         <>
-            <h2 className={styles.subMenu} id="bubbleTea">{data.data.restaurant.bubbleTea.title}</h2>
+            <section section className={styles.bubbleTeaContainer_content}>
+            <h2 className={styles.bubbleTeaContainer_title} id="bubbleTea">{data.data.restaurant.bubbleTea.title}</h2>
             <GatsbyImage 
                image={background} 
                alt={'bubbeTea-image'}
                layout={"fullWidth"}
                placeholder={"blurred"}
+               style={
+                   {
+                       opacity:'50%',
+                       position:'absolute',
+                       width:'100%',
+                       height:'100%'
+                   }
+               }
             />
-            <div>
-                <div>
-                    <h4>{data.data.restaurant.bubbleTea.teas.title}</h4>
-                    <ul >
-                        {
-                            data.data.restaurant.bubbleTea.teas.content.map((item)=>{
-                                return(
-                                    <li key={uuidv4()}>{item}</li>
-                                )
-                            })
-                        }
-                    </ul>
+                <div className={styles.bubbleTeaContainer_two}>
+                    <div>
+                        <h4 className={styles.bubbleTeaText_subtitle}>{data.data.restaurant.bubbleTea.teas.title}</h4>
+                        <ul >
+                            {
+                                data.data.restaurant.bubbleTea.teas.content.map((item)=>{
+                                    return(
+                                        <li  
+                                            key={uuidv4()}
+                                        >{item}</li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    </div>
+                    <div>
+                        <h4 className={styles.bubbleTeaText_subtitle}>{data.data.restaurant.bubbleTea.juices.title}</h4>
+                        <ul>
+                            {
+                                data.data.restaurant.bubbleTea.juices.content.map((item)=>{
+                                    return(
+                                        <li key={uuidv4()}>{item}</li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    </div>
                 </div>
-                <div>
-                    <h4>{data.data.restaurant.bubbleTea.juices.title}</h4>
-                    <ul>
+                <div className={styles.bubbleTeaContainer_one}>
+                    <div>
+                        <h4 className={styles.bubbleTeaText_subtitle2}>{data.data.restaurant.bubbleTea.addOns.title}</h4>
+                        <ul>
+                            {
+                                data.data.restaurant.bubbleTea.addOns.content.map((item)=>{
+                                    return(
+                                        <li key={uuidv4()}>{item}</li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    </div>
+                    <div>
+                        <h4 className={styles.bubbleTeaText_subtitle2}>{data.data.restaurant.bubbleTea.milkSubstitutes.title}</h4>
+                        <ul>
+                            {
+                                data.data.restaurant.bubbleTea.milkSubstitutes.content.map((item)=>{
+                                    return(
+                                        <li key={uuidv4()}>{item}</li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    </div>
+                    <GatsbyImage 
+                       image={img} 
+                       alt={'bubbeTea-image'}
+                       layout={"fullWidth"}
+                       placeholder={"blurred"}
+                       style={
                         {
-                            data.data.restaurant.bubbleTea.juices.content.map((item)=>{
-                                return(
-                                    <li key={uuidv4()}>{item}</li>
-                                )
-                            })
+                            width:'300px',
+                            height:'300px',
+                            marginLeft:'15%'
                         }
-                    </ul>
+                    }
+                    />
                 </div>
-            </div>
-            <div>
-                <div>
-                    <h4>{data.data.restaurant.bubbleTea.addOns.title}</h4>
-                    <ul>
-                        {
-                            data.data.restaurant.bubbleTea.addOns.content.map((item)=>{
-                                return(
-                                    <li key={uuidv4()}>{item}</li>
-                                )
-                            })
-                        }
-                    </ul>
-                </div>
-                <div>
-                    <h4>{data.data.restaurant.bubbleTea.milkSubstitutes.title}</h4>
-                    <ul>
-                        {
-                            data.data.restaurant.bubbleTea.milkSubstitutes.content.map((item)=>{
-                                return(
-                                    <li key={uuidv4()}>{item}</li>
-                                )
-                            })
-                        }
-                    </ul>
-                </div>
-                <GatsbyImage 
-                   image={img} 
-                   alt={'bubbeTea-image'}
-                   layout={"fullWidth"}
-                   placeholder={"blurred"}
-                />
-            </div>
+            </section>
         </>
     )
 }
